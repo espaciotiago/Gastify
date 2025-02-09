323d046b41176b0665cff77987bc8e557d9b51d2
Santiago Moreno <msantim@hotmail.com>
2025-02-09 18:43:15 -0500

    Rollback

--- GastifyTests/GastifyTests.swift
+++ GastifyTests/GastifyTests.swift
@@ -10,9 +10,23 @@ import XCTest
 
 final class GastifyTests: XCTestCase {
 
-    func testEjemplo() {
-        let resultado = 2 + 2
-        XCTAssertEqual(resultado, 4, "La suma no es correcta")
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
     }
 
 }
